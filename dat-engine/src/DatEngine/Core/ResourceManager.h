7848f71bbfbe14d6e34f455c2db65f1a737238cc
Paulo Castro <paulovicg@outlook.com>
2022-10-21 18:21:40 -0400

    Adds a resource manager

---
+++ dat-engine/src/DatEngine/Core/ResourceManager.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "pch.h"
+#include "Graphics/Shader.h"
+#include "Graphics/Texture.h"
+
+namespace dat
+{
+	class ResourceManager
+	{
+	public:
+		static void clear();
+
+	public:
+		static Shader loadShader(std::string_view shaderName, const char* vertexShaderFile, const char* fragmentShaderFile);
+
+		static Shader getShader(std::string_view shaderName);
+
+		static Texture2D loadTexture(std::string_view textureName, const char* textureFile, bool alpha);
+
+		static Texture2D getTexture(std::string_view textureName);
+
+	private:
+		static Shader loadShaderFromFile(const char* vertexShaderFile, const char* fragmentShaderFile);
+
+		static Texture2D loadTextureFromFile(const char* textureFile, bool alpha);
+
+	private:
+		static inline std::unordered_map<std::string_view, Shader> m_Shaders = {};
+		static inline std::unordered_map<std::string_view, Texture2D> m_Textures = {};
+	};
+}
