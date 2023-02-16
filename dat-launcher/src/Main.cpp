4ad7413f063c3d491ee9b42fa3df18aab8a83bda
Paulo Castro <paulovicg@outlook.com>
2023-02-16 18:51:28 -0400

    Takes the transform out

--- dat-launcher/src/Main.cpp
+++ dat-launcher/src/Main.cpp
@@ -1,14 +1,5 @@
 #include <DatEngine.h>
 
-struct Transform
-{
-	glm::vec3 position{ 0.f };
-	glm::vec3 eulerRotation{ 0.f };
-	glm::vec3 scale = { 1.0f, 1.0f, 1.0f };
-
-	glm::mat4 modelMatrix = glm::mat4(1.0f);
-};
-
 void main()
 {
 	auto app = dat::core::DatApplication(800, 600, "Title");
