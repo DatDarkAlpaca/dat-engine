0a34e14aeac5946217475b934932d4fb5b867eea
Paulo Castro <paulovicg@outlook.com>
2023-03-28 21:52:54 -0400

    Moves the shader instantiation to another file

---
+++ dat-engine/src/Graphics/OpenGL/Factories/ShaderFactory.h
@@ -0,0 +1,90 @@
+#pragma once
+#include "pch.h"
+
+namespace
+{
+#ifdef DAT_DEBUG
+	inline std::string shaderTypeStr(unsigned int shaderType)
+	{
+		switch (shaderType)
+		{
+		case GL_VERTEX_SHADER:
+			return "Vertex";
+		case GL_FRAGMENT_SHADER:
+			return "Fragment";
+		case GL_GEOMETRY_SHADER:
+			return "Geometry";
+		default:
+			return "Unknown Shader";
+		}
+	}
+#endif
+	
+	inline unsigned int compileShader(const char* filepath, unsigned int type)
+	{
+		unsigned int shader = glCreateShader(type);
+		std::string vertexSource = dat::readFile(filepath);
+
+		const char* vertexSrc = vertexSource.c_str();
+		glShaderSource(shader, 1, &vertexSrc, NULL);
+		glCompileShader(shader);
+
+		return shader;
+	}
+
+	inline void checkCompileErrors(unsigned int object, unsigned int type)
+	{
+		int success;
+		char infoLog[1024];
+
+		glGetShaderiv(object, GL_COMPILE_STATUS, &success);
+
+		if (!success)
+		{
+			glGetShaderInfoLog(object, 512, NULL, infoLog);
+
+			DAT_CORE_ERROR("{} Shader Error:\n{}", shaderTypeStr(type), infoLog);
+
+			glDeleteShader(object);
+		}
+	}
+
+	inline void checkLinkErrors(unsigned int object)
+	{
+		int success;
+		char infoLog[1024];
+
+		glGetProgramiv(object, GL_LINK_STATUS, &success);
+
+		if (!success)
+		{
+			glGetProgramInfoLog(object, 1024, nullptr, infoLog);
+			DAT_CORE_ERROR("Linking Error:\n{}", infoLog);
+
+			glDeleteProgram(object);
+		}
+	}
+}
+
+inline unsigned int createShader(const char* vertexShaderPath, const char* fragmentShaderPath)
+{
+	unsigned int vertexShader = compileShader(vertexShaderPath, GL_VERTEX_SHADER);
+	checkCompileErrors(vertexShader, GL_VERTEX_SHADER);
+
+	unsigned int fragmentShader = compileShader(fragmentShaderPath, GL_FRAGMENT_SHADER);
+	checkCompileErrors(fragmentShader, GL_FRAGMENT_SHADER);
+
+	unsigned int program = glCreateProgram();
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	checkLinkErrors(program);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return program;
+}
